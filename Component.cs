5ecd7a0d56519ced785eb1fe92bc7dbbca4f9064
flslin <84002788+flslin@users.noreply.github.com>
2024-03-18 16:56:52 +0900

    플레이어 이동

--- Component.cs
+++ Component.cs
@@ -21,6 +21,6 @@
 
     }
 
-    public GameObject? gameObject; // 내가 어디있는지 확인하는 용도
-    public Transform? transform; // 내가 속해 있는 게임오브젝트의 이동을 처리하는 용도
+    public GameObject gameObject; // 내가 어디있는지 확인하는 용도
+    public Transform transform; // 내가 속해 있는 게임오브젝트의 이동을 처리하는 용도
 }
---
+++ PlayerController.cs
@@ -0,0 +1,40 @@
+﻿
+class PlayerController : Component
+{
+    Engine engine;
+
+    public override void Start() // 자식이 재정의 할수도 있음을 표시
+    {
+
+    }
+
+    public override void Update()
+    {
+        if (Input.GetButton("Up"))
+        {
+            transform.y--;
+        }
+        if (Input.GetButton("Left"))
+        {
+            transform.x--;
+        }
+        if (Input.GetButton("Down"))
+        {
+            transform.y++;
+        }
+        if (Input.GetButton("Right"))
+        {
+            transform.x++;
+        }
+        if (Input.GetButton("Quit"))
+        {
+            // singleton pattern : 오브젝트가 엔진꺼 써야할 필요가 있을 때 씀
+            engine.Stop();
+        }
+
+        transform.x = Math.Clamp(transform.x, 0, 80);
+        transform.y = Math.Clamp(transform.y, 0, 80);
+    }
+
+
+}
