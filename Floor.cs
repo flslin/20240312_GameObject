1cf28421b72553593d1e5bef55a8f0e3f570ba6c
flslin <84002788+flslin@users.noreply.github.com>
2024-03-18 16:57:27 +0900

    이전 내용

---
+++ Floor.cs
@@ -0,0 +1,39 @@
+﻿
+class Floor : GameObject
+{
+    public Floor()
+    {
+        shape = ' ';
+        layerOrder = 10;
+    }
+
+    public Floor(int newX, int newY) // 생성자 오버로드
+    {
+        shape = ' ';
+
+        x = newX;
+        y = newY;
+        layerOrder = 10;
+    }
+
+    ~Floor()
+    {
+
+    }
+
+    public override void Start()
+    {
+
+    }
+
+    public override void Update()
+    {
+
+    }
+
+    public override void Render()
+    {
+        base.Render();
+    }
+
+}
---
+++ Goal.cs
@@ -0,0 +1,38 @@
+﻿
+class Goal : GameObject
+{
+    public Goal()
+    {
+        shape = 'G';
+        layerOrder = 100;
+    }
+    public Goal(int newX, int newY) // 생성자 오버로드
+    {
+        shape = 'G';
+
+        x = newX;
+        y = newY;
+        layerOrder = 100;
+    }
+
+    ~Goal()
+    {
+
+    }
+
+    public override void Start()
+    {
+
+    }
+
+    public override void Update()
+    {
+
+    }
+
+    public override void Render()
+    {
+        base.Render();
+    }
+
+}
